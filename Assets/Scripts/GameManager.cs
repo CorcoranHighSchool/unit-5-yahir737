0b2b1f289afc912e867397da23ccf624ebe189dd
yahir737 <142628989+yahir737@users.noreply.github.com>
2024-02-28 12:59:00 -0800

    5.3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Button restartButton;
+    public TextMeshProUGUI gameOverText;
     public static GameManager instance;
     private float spawnRate = 1.0f;
     public List<GameObject> targets;
     private int score;
     public TextMeshProUGUI scoreText;
+    public bool isGameActive {get; private set;}
     private void Awake(){
         instance = this;
     }
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
+        gameOverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+    }
+    public void GameOver(){
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
     }
     public void UpdateScore( int scoreToAdd)
     {
@@ -25,7 +42,7 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
     IEnumerator SpawnTarget(){
-        while(true){ 
+        while(isGameActive){ 
             yield return new WaitForSeconds(spawnRate);
             //spawn a target
             int index = Random.Range(0, targets.Count);
